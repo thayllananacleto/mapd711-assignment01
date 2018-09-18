91e2e7402d5bdb6f92a6ae1b8872e3979e5bc778
Thayllan Anacleto <thayllananacleto@Thayllans-MacBook-Pro.local>
2018-09-18 03:17:14 -0400

    Inserting Functions

--- app/src/main/java/com/example/thayllananacleto/assignment/MainActivity.java
+++ app/src/main/java/com/example/thayllananacleto/assignment/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.thayllananacleto.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SendForm(View view) {
+        //Instantiate Intent Class
+        Intent intent = new Intent(MainActivity.this, SubActivity.class);
+
+        //Sending the form
+        EditText etFullName = findViewById(R.id.etFullName);
+        intent.putExtra("fullName", etFullName.getText().toString());
+
+        EditText etQualification = findViewById(R.id.etQualification);
+        intent.putExtra("qualification", etQualification.getText().toString());
+
+        EditText etProfession = findViewById(R.id.etProfession);
+        intent.putExtra("profession", etProfession.getText().toString());
+
+        EditText etHobby = findViewById(R.id.etHobby);
+        intent.putExtra("hobby", etHobby.getText().toString());
+
+        EditText etDreamJob = findViewById(R.id.etDreamJob);
+        intent.putExtra("dreamJob", etDreamJob.getText().toString());
+
+        //Go to Sub Activity
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/thayllananacleto/assignment/SubActivity.java
+++ app/src/main/java/com/example/thayllananacleto/assignment/SubActivity.java
@@ -1,7 +1,9 @@
 package com.example.thayllananacleto.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        //Initialize
+        Intent i = getIntent();
+
+        String fullName = i.getStringExtra("fullName");
+        String qualification = i.getStringExtra("qualification");
+        String profession = i.getStringExtra("profession");
+        String hobby = i.getStringExtra("hobby");
+        String dreamJob = i.getStringExtra("dreamJob");
+
+        //Defining a Java reference object for a TextView
+        TextView tvFullName = findViewById(R.id.tvFullName);
+        tvFullName.setText(fullName);
+
+        TextView tvQualification = findViewById(R.id.tvQualification);
+        tvQualification.setText(qualification);
+
+        TextView tvProfession = findViewById(R.id.tvProfession);
+        tvProfession.setText(profession);
+
+        TextView tvHobby = findViewById(R.id.tvHobby);
+        tvHobby.setText(hobby);
+
+        TextView tvDreamJob = findViewById(R.id.tvDreamJob);
+        tvDreamJob.setText(dreamJob);
     }
 }
